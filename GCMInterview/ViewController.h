e101be189badc25cfb75655c6a0624fe392eb7a7
ericpass <ecp89@cornell.edu>
2016-02-01 15:56:57 -0500

    Initial Commit

---
+++ GCMInterview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCMInterview
+//
+//  Created by Eric Pass on 2/1/16.
+//  Copyright © 2016 Eric Pass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCMInterview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCMInterview
+//
+//  Created by Eric Pass on 2/1/16.
+//  Copyright © 2016 Eric Pass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
