52d8ea0105e81ff76bcaf1b139ebafd81d76965b
ericpass <ecp89@cornell.edu>
2016-02-07 13:33:07 -0500

    Starting to work on mutual friends traversing

---
+++ GCMInterview/TestFriend.h
@@ -0,0 +1,15 @@
+//
+//  TestFriend.h
+//  GCMInterview
+//
+//  Created by Eric Pass on 2/7/16.
+//  Copyright © 2016 Eric Pass. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestFriend : NSObject
+@property NSString * uid;
+@property NSString * name;
+@property NSMutableArray *mutualFriends;
+@end
---
+++ GCMInterview/TestFriend.m
@@ -0,0 +1,15 @@
+//
+//  TestFriend.m
+//  GCMInterview
+//
+//  Created by Eric Pass on 2/7/16.
+//  Copyright © 2016 Eric Pass. All rights reserved.
+//
+
+#import "TestFriend.h"
+
+@implementation TestFriend
+@synthesize uid;
+@synthesize name;
+@synthesize mutualFriends;
+@end
